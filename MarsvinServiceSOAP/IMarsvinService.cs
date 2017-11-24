9bd16e0f880374dc11801d6f4ad01a540d382f78
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-11-24 13:03:41 +0100

    Add project files.

---
+++ MarsvinServiceSOAP/IMarsvinService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MarsvinServiceSOAP
+{
+    [ServiceContract]
+    public interface IMarsvinService
+    {
+        [OperationContract]
+        IList<User> GetAllUsers();
+
+
+        [OperationContract]
+        IList<User> GetUserByPhoneNo(int phoneno);
+
+        [OperationContract]
+        IList<User> GetUserByMail(string email);
+
+        [OperationContract]
+        int AddUser(User user);
+
+        [OperationContract]
+        void DeleteUser(User user);
+
+
+    }
+
+}
